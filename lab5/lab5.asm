6b7bcaf2dd898f620c5471130f5ad0732938e1e9
The-Dankest <ryanlongacre@outlook.com>
2017-09-30 20:10:18 -0700

    Removed debug info, added winning messages and guess counter, added 'To high/To low' guess indicators.

--- lab5/lab5.asm
+++ lab5/lab5.asm
@@ -29,37 +29,49 @@ _main:
 									;  which is too big)
 	add		dword [int_magic_number], 1	; add 1 to make it in range 1-100
 	
-	; for debugging, prints magic number
-	; TODO: remove when lab is finished
-	push	dword [int_magic_number]
-	push	str_printfmt
-	call	_printf
-	add		esp, 8
-	
 while_start:
 	; prompt, call printf
 	push	str_intro
 	call	_printf
 	add		esp, 4
-	; get input, call scanf
+	; get input, call scanf, increment guess count
 	push	int_guess
 	push	str_scanfmt
 	call	_scanf
-	add		esp, 8
-	
-	; for testing, echo input
-	; TODO remove this part AND the fmt string var
-	push	dword [int_guess]
-	push	str_printfmt
-	call	_printf
+	inc		dword [int_num_guesses]
 	add		esp, 8
 	
 	; compare guess and magic number
-	mov		ebx, [int_guess]
-	cmp		ebx, [int_magic_number]
+	mov		ebx, [int_magic_number]
+	cmp		ebx, [int_guess]
 	je		exit_loop
+	jl		less_than
+	jmp		greater_than
+	
+less_than:
+	; print less-than message, then jump to loop begin
+	push	str_toohigh
+	call	_printf
+	add		esp, 4
 	jmp		while_start
+
+greater_than:
+	; print greater-than message, then jump to loop begin
+	push	str_toolow
+	call	_printf
+	add		esp, 4
+	jmp		while_start
+
 exit_loop:
+	
+	; print win message and number of guesses
+	push	str_correct
+	call	_printf
+	add		esp, 4
+	push	dword [int_num_guesses]
+	push	str_guessnum
+	call	_printf
+	add		esp, 8
 	
 	; return 0
 	mov		eax, 0
@@ -72,10 +84,11 @@ section .data
 
 str_intro	db	"I'm thinking of a number between 1 and 100; what is it? ", 0
 str_correct	db	"You guessed it! Thanks for playing...", 10, 0
+str_guessnum	db	"It took you %d guesses.", 10, 0
 str_toolow	db	"Too low!", 10, 0
 str_toohigh	db	"Too high!", 10, 0
 str_scanfmt	db	"%d", 0
-str_printfmt	db	"Input: %d", 10, 0
 
 int_magic_number	dd	0
 int_guess	dd	0
+int_num_guesses		dd	0
