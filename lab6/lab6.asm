cc0cf16eab36b51a1eba309ae866187bfcfb6366
The-Dankest <ryanlongacre@outlook.com>
2017-10-11 21:12:27 -0700

    Finished recursion and modified report.

--- lab6/lab6.asm
+++ lab6/lab6.asm
@@ -8,29 +8,72 @@ extern _scanf
 
 section .text
 
-global _main
-
+global _hanoi
 _hanoi:
 	; recursive function for solving Tower of Hanoi
-	; TODO implement recursion
+	; hanoi(int disk, int src, int dst, int tmp)
+	; parameters:
+	;	disk: [ebp + 8]
+	;	src: [ebp + 12]
+	;	dst: [ebp + 16]
+	;	tmp: [ebp + 20]
+
 	push	ebp
 	mov		ebp, esp
 	
-	; PoC - print first three params
+	; check if we should break or recurse
+	cmp		dword [ebp + 8], 1
+	je		_true
+	jmp		_false
+		
+_true:
+	; printf("Move disk 1 from %d to %d\n", src, dst);
+	push	dword [ebp + 16]
+	push	dword [ebp + 12]
+	push	1d
+	push	str_move
+	call	_printf
+	add		esp, 16
+
+	mov		esp, ebp
+	pop		ebp
+	ret
+
+_false:
+	; move num_disks to ecx and decrement by 1
+	mov		ecx, dword [ebp + 8]
+	dec		ecx
+	push	ecx
+	
+	; hanoi(disk - 1, src, tmp, dst);
+	push 	dword [ebp + 16]
+	push	dword [ebp + 20]
+	push	dword [ebp + 12]
+	push	dword [ebp - 4]
+	call	_hanoi
+	add		esp, 16
+
+	; printf("Move disk %d from %d to %d\n", disk, src, dst);
 	push 	dword [ebp + 16]
 	push	dword [ebp + 12]
 	push	dword [ebp + 8]
 	push	str_move
 	call	_printf
 	add		esp, 16
-	
-	; PoC - return fourth to print
-	mov		eax, [ebp + 20]
-	
+
+    ; hanoi(disk - 1, tmp, dst, src);
+	push 	dword [ebp + 12]
+	push	dword [ebp + 16]
+	push	dword [ebp + 20]
+	push	dword [ebp - 4]
+	call	_hanoi
+	add		esp, 16
+
 	mov		esp, ebp
 	pop		ebp
 	ret
 
+global _main
 _main:
 	; Boilerplate "function prologue"
 	push	ebp
@@ -39,13 +82,12 @@ _main:
 	; prompt, call printf
 	push	str_prompt
 	call	_printf
-	; don't clean off the stack because we are reserving space for num_disks
+	; don't clean off the stack because we are reserving space for num_disks (at [ebp - 4])
 	lea		ecx, [ebp - 4]
 	push 	ecx
 	push 	str_scanfmt
 	call 	_scanf
 	add 	esp, 8
-	; num_disks is at ebp - 4
 	
 	cmp 	dword eax, 1
 	jne		invalid
@@ -63,24 +105,12 @@ invalid:
 	ret
 	
 valid:
-	; TODO remove PoC stuff
-	; TODO call _hanoi(num_disks, 1, 2, 3) once recursive _hanoi works
-	; PoC - call _hanoi(num_disks, 64, 128, 42)
-	; in _hanoi, print first 3 params
-	; return fourth
-	push	42d
-	push	128d
-	push	64d
+	; call _hanoi(num_disks, 1, 2, 3)
+	push	3d
+	push	2d
+	push	1d
 	push	dword [ebp - 4]
 	call	_hanoi
-	add		esp, 16
-	
-	; PoC - print return value of hanoi (though hanoi won't return anything in final ver.)
-	push 	0d
-	push	0d
-	push	eax
-	push	str_move
-	call	_printf
 	add		esp, 16
 	
 	; Boilerplate "function epilogue"/return
@@ -96,5 +126,3 @@ str_move	db	"Move disk %d from %d to %d", 10, 0
 str_prompt	db	"How many disks do you want to play with? ", 0
 str_error	db	"Uh-oh, I couldn't understand that...  No towers of Hanoi for you!", 10, 0
 str_scanfmt	db	"%d", 0
-; TODO remove hello string
-hello		db "Hello", 10, 0
